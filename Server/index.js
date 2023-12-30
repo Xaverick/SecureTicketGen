05fa5dc72a83f2fd34ac0e45a389dab3f5e6626d
kartik <kartikaggarwal2004@gmail.com>
2023-12-30 09:17:35 +0530

    updated CORS

--- Server/index.js
+++ Server/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 })
 
 
-app.use(cors({ origin: `${process.env.ORIGIN}`, credentials: true}));
+app.use(cors({ origin: [`${process.env.ORIGIN}`, "https://secure-ticket-gen.vercel.app"], credentials: true}));
 app.use (cookieParser(process.env.SECRET));
 app.use(express.json());
 
