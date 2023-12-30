789f69f30f75f255e94d83984538dc363bc009fe
Yash Mittal <mittalyas1234@gmail.com>
2023-12-30 07:56:07 +0530

    home page final

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,14 +1,15 @@
-import React, {useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import './Home.scss'
-const {useNavigate} = require('react-router-dom');
-const {Context} = require('../../utils/context');
+import gamelogo from '../../assets/game image.jpeg'
+const { useNavigate } = require('react-router-dom');
+const { Context } = require('../../utils/context');
 
 
 
 const Home = () => {
   const navigate = useNavigate();
-  const {userInfo, updateUser, setUserInfo} = useContext(Context);
+  const { userInfo, updateUser, setUserInfo } = useContext(Context);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,18 +31,18 @@ const Home = () => {
     };
 
     fetchData();
-  }, []); 
-  
+  }, []);
+
   console.log(userInfo);
 
   const handleTicketButton = async () => {
-    try{
+    try {
       const response = await fetch(`${process.env.REACT_APP_BACKEND}/generateQRCode`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username: userInfo.username , email: userInfo.email}),
+        body: JSON.stringify({ username: userInfo.username, email: userInfo.email }),
         credentials: 'include', // Include cookies in the request
       });
 
@@ -62,7 +63,7 @@ const Home = () => {
       alert("Ticket not issued");
       console.error(error);
     }
-    
+
   }
 
   return (
@@ -71,18 +72,33 @@ const Home = () => {
       <div className="home">
         <div className="home-left">
           <h1>Game page photo</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae ut nesciunt delectus
-            ea temporibus magni repudiandae deleniti tenetur inventore itaque? Maiores doloribus similique commodi, perspiciatis
-            repudiandae non a. Doloribus, libero.</p>
-          <h2>game page link</h2>
-          <img src='' alt='' />
+          <img src={gamelogo} alt='' />
+          <h2>Soon it will be provided ...</h2>
           <a href=''></a>
         </div>
         <div className="home-right">
           <h1> Game Rules</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam obcaecati
-            dolores beatae necessitatibus odio est laborum illo maxime autem quos, voluptatibus, dolorum
-            nesciunt iste officiis asperiores molestiae repellat, modi delectus?</p>
+          <p>
+            1. Each player is limited to a single attempt at the game.
+            This ensures equal opportunities and prevents one player from dominating the leaderboard through multiple plays.
+          </p>
+          <br />
+          <p>
+            2. Players earn points by shooting down obstacles.
+            The scoring system could be based on factors such as the number of obstacles shot, their difficulty, or a combination of these elements.
+          </p>
+          <br />
+          <p>
+            3. The game is open for a specific period, creating a sense of urgency.
+            Players need to participate within this timeframe to be eligible for rewards.
+          </p>
+          <br />
+          <p>
+            4. The leaderboard displays the top 5 players based on their scores.
+            Rewards or discount coupons are allocated to these top 5 players.
+          </p>
+          <br />
+          <br />
           <button className='button' onClick={handleTicketButton}>Get your Tickect</button>
         </div>
       </div>
